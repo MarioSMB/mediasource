04b77332ccc8569c46519c258785b40ab32eee0e
Mario <mario@smbclan.net>
2018-10-16 22:46:39 +1000

    Add sources for Uzi

---
+++ mod-weapons/sources/mac10/s.bat
@@ -0,0 +1,5 @@
+iqm v_mac10.iqm v_mac10.SMD
+copy v_mac10.iqm c:\Xonotic\data\models\weapons\v_uzi.md3 /y
+iqm g_mac10.iqm g_mac10.SMD
+copy g_mac10.iqm c:\Xonotic\data\models\weapons\g_uzi.md3 /y
+pause
---
+++ mod-weapons/sources/mac10/z.bat
@@ -0,0 +1,4 @@
+dpmodel mac10.txt
+copy h_mac10.dpm c:\Xonotic\data\models\weapons\h_uzi.iqm /y
+copy h_mac10.dpm.framegroups c:\Xonotic\data\models\weapons\h_uzi.iqm.framegroups /y
+pause
